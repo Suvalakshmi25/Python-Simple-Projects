1bd896c21087b4e80a1272d1a40b04192a23ee15
Suvalakshmi S <140824003+Suvalakshmi25@users.noreply.github.com>
2023-07-28 23:28:38 +0530

    Add files via upload

---
+++ Etch-A-Sketch.py
@@ -0,0 +1,39 @@
+import turtle as tr
+tim=tr.Turtle()
+screen = tr.Screen()
+
+screen.setup(700,700)
+screen.title("Etch-A-Sketch")
+screen.bgcolor("black")
+tim.pencolor("white")
+def move_forward():
+    tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+def turn_left():
+    new_heading=tim.heading()+10
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading=tim.heading()-10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forward,"w")
+screen.onkey(move_backwards,"s")
+screen.onkey(turn_left,"a")
+screen.onkey(turn_right,"d")
+
+screen.onkey(clear,"c")
+
+
+
+
+
+
+screen.exitonclick()
